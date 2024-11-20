53913d8678b219176d7e6e399577f9db2f217716
Dmondes <desmondlimws@gmail.com>
2024-11-20 10:48:31 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/iss/vttp5a__ssf_day13/Vttp5aSsfDay13Application.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.vttp5a__ssf_day13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vttp5aSsfDay13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vttp5aSsfDay13Application.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/vttp5a__ssf_day13/Vttp5aSsfDay13ApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.vttp5a__ssf_day13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vttp5aSsfDay13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
